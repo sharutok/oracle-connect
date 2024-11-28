02809529ed6a224d7b27e0537d1fd303eb420f55
Sharan Kudtarkar <SK19@ador.co.in>
2024-11-28 23:14:08 +0530

    28-11-2024---23-13-34

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
@@ -23,9 +23,9 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	var cred Cred
-	cred.Username = os.Getenv("APP_USERNAME")
-	cred.Password = os.Getenv("APP_PASSWORD")
+	// var cred Cred
+	// cred.Username = os.Getenv("APP_USERNAME")
+	// cred.Password = os.Getenv("APP_PASSWORD")
 
 	app := fiber.New()
 	app.Use(logger.New())
@@ -34,11 +34,19 @@ func main() {
 		AllowMethods: "GET,POST",
 	}))
 
-	app.Use(basicauth.New(basicauth.Config{
-		Users: map[string]string{
-			cred.Username: cred.Password,
-		},
-	}))
+	app.Use(func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+		log.Println((authHeader[7:]), os.Getenv("APP_PASSWORD"), len(authHeader[7:]), len(os.Getenv("APP_PASSWORD")), authHeader[7:] == os.Getenv("APP_PASSWORD"), authHeader[:7] == "Bearer ")
+		if authHeader[7:] == os.Getenv("APP_PASSWORD") && authHeader[:7] == "Bearer " {
+			token := authHeader[7:]
+			fmt.Println("Bearer Token:", token)
+			return c.Next()
+		}
+		return c.Status(500).JSON(&fiber.Map{
+			"mess": "something went wrong",
+		})
+	})
+
 	app.Post("/query-execute", QueryExecute)
 	app.Get("/health-check", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(&fiber.Map{
